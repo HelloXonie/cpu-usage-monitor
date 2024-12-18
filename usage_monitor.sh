12b323df3e7ac56533ab6898760f9113ebba66d8
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2024-12-18 11:27:08 -0800

    updated script now has a disk usage portion

--- usage_monitor.sh
+++ usage_monitor.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
+#define log file and values
 LOG_FILE="/var/log/cpu_usage.log"
+DISK_THRESHOLD=90
+
 DATE=$(date +'%Y-%m-%d %H:%M:%S')
+
 CPU_USAGE=$(top -bn1 | grep "%Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
+
+#log cpu usage with a timestamp
 echo "$DATE - CPU Usage: $CPU_USAGE%" >> $LOG_FILE
+
+#check disk usage
+DISK_USAGE=$(df / | grep / | awk '{ print $5 }' | sed 's/%//g') 
+if [ $DISK_USAGE -gt $DISK_THRESHOLD ]; then
+	ALERT="Disk usage is high: $DISK_USAGE%"
+	echo "$DATE - ALERT: $ALERT" >> $LOG_FILE
+else
+	echo "Disk usage is normal: $DISK_USAGE" >> $LOG_FILE
+fi
